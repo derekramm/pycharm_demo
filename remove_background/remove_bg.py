bd1e18ca2526543590a7fe261dd4855d32b4cace
derek_ramm <derek_ramm@icloud.com>
2019-08-29 17:32:00 +0800

    clear

--- remove_background/remove_bg.py
+++
@@ -1,8 +0,0 @@
-from removebg import RemoveBg
-import os
-
-rmbg = RemoveBg('DNPfUtzXVwGtqTatMzu2q8MK', 'error.log')
-
-path = f'{os.getcwd()}/images'
-for img in os.listdir(path):
-    rmbg.remove_background_from_img_file(f'{path}/{img}')
--- wordcloud/readme.txt
+++
@@ -1,3 +0,0 @@
-get_comment.py:获取影评信息
-participle.py:使用jieba分词
-wordcloud.py:生成云图
